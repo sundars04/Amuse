ef20d99cb073b1ac9ef2283b642ce9e68c35581d
Sundars0 <Sundars04@outlook.com>
2016-12-30 05:22:43 +0000

    generate User with devise, modify migration to include confirmable modify post controller to authenticate and user post creation. create navbar partial and flash partial.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  link        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Post < ApplicationRecord
+  belongs_to :user
+
+  # Sexy Validations
+  validates :title, :presence => true, 
+                    :length   => { :minimum => 3 }
+  scope :sorted, -> { order("created_at Desc") }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  link        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
