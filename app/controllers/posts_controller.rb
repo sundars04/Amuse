4ef44c9dcdb464689a2285d682b90bbe3523c517
Sundars0 <Sundars04@outlook.com>
2016-12-30 06:06:41 +0000

    add image to post, modify show and index to show images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :link, :description)
+      params.require(:post).permit(:title, :link, :description, :image)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,17 @@
 #
 # Table name: posts
 #
-#  id          :integer          not null, primary key
-#  title       :string
-#  link        :string
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  user_id     :integer
+#  id                 :integer          not null, primary key
+#  title              :string
+#  link               :string
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  user_id            :integer
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Post < ApplicationRecord
@@ -17,5 +21,8 @@ class Post < ApplicationRecord
   # Sexy Validations
   validates :title, :presence => true, 
                     :length   => { :minimum => 3 }
+
   scope :sorted, -> { order("created_at Desc") }
+  has_attached_file :image, styles: { medium: "700x500>", small: "350x250>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
